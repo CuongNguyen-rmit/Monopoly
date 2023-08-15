7f47d35acc018ef954ebf49bc73d572ecf5b6cf7
CuongNguyen-rmit <s3891758@rmit.edu.vn>
2023-08-15 11:29:42 +0700

    Initial Commit

---
+++ Monopoly/MonopolyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonopolyApp.swift
+//  Monopoly
+//
+//  Created by Cuong Nguyen on 15/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct MonopolyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
